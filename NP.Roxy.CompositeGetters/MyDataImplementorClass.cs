c47f68fc4497cfebb86605162e751f3c3c61c842
npolyak <nick.polyak@verizon.net>
2018-02-23 14:11:52 -0500

    added CompositeGetters project.

---
+++ NP.Roxy.CompositeGetters/MyDataImplementorClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Roxy.CompositeGetters
+{
+    public abstract class MyDataImplementorClass
+    {
+        public abstract string FirstName { get; }
+
+        public abstract string LastName { get; }
+
+        public string FullName1
+        {
+            get => $"{LastName}, {FirstName}";
+        }
+    }
+}
