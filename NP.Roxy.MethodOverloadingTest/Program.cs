e59d0d18a269539f5bb189bd9bd02b4d7ad5546c
npolyak <nick.polyak@verizon.net>
2018-02-25 03:45:21 -0500

    added overloading sample

---
+++ NP.Roxy.MethodOverloadingTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP.Roxy.MethodOverloadingTest
+{
+    public interface IWrapper
+    {
+        MyDataImpl TheDataImpl { get; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core.SetSaveOnErrorPath("GeneratedCode");
+
+            ITypeConfig<IWrapper> typeConfig = Core.FindOrCreateTypeConfig<IMyData, IWrapper>();
+
+            typeConfig.ConfigurationCompleted();
+
+            IMyData myData = Core.GetInstanceOfGeneratedType<IMyData>();
+
+            myData.FirstName = "Joe";
+            myData.LastName = "Doe";
+
+            string greetingStr1 = myData.GetGreeting();
+            Console.WriteLine(greetingStr1);
+
+            string greetingStr2 = myData.GetGreeting("Hello");
+
+            Console.WriteLine(greetingStr2);
+
+            Core.Save("GeneratedCode");
+        }
+    }
+}
